5cb5481a3d1ef32fa0c091b0e9698d514404ecc5
Jade <jadecdrb@gmail.com>
2021-03-03 11:06:46 -0500

    adjustmet

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import '../syles/App.css';
 import TodoList from './TodoList';
 import LastItemDisplay from './LastItemDisplay';
+import axios from 'axios'
 
 function App() {
+
+  useEffect(() => {
+    console.log('v1.01')
+    axios
+      .get('http://localhost:8080/api/users')
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+      
+  }, [])
   // testing
   //asasdfsjadhfl adsjfl kadsjkf
   return (
